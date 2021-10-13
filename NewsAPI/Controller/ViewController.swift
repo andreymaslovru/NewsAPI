599bce28e3550b8d887eeca13fd94a0b3f0e075d
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-13 12:47:10 +0700

    added sheetController on click article

--- NewsAPI/Controller/ViewController.swift
+++ NewsAPI/Controller/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
         
         apiService.getNewsData { (result) in
             print(result)
@@ -38,17 +39,29 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! NewsTableViewCell
-        
+
         let article = viewModel.cellForRowAt(indexPath: indexPath)
         cell.selCellWithValuesOf(article)
-        
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.numberOfRowsInSection(section: section)
     }
-    
 }
+
+extension ViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            let sheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            sheet.addAction(UIAlertAction(title: "Go to a web", style: .default, handler: { _ in
+                print("go to the website")
+            }))
+            self.present(sheet, animated: true)
+        }
+}
+
 
 
--- NewsAPI/View/NewsTableViewCell.swift
+++ NewsAPI/View/NewsTableViewCell.swift
@@ -7,20 +7,19 @@
 
 import UIKit
 
-class NewsTableViewCell: UITableViewCell {
+class NewsTableViewCell: UITableViewCell{
+
     @IBOutlet weak var titleNews: UILabel!
     @IBOutlet weak var descriptionNews: UILabel!
     @IBOutlet weak var imageNews: UIImageView!
     
 //    override func awakeFromNib() {
 //        super.awakeFromNib()
-//        // Initialization code
 //    }
 //
 //    override func setSelected(_ selected: Bool, animated: Bool) {
 //        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
+//        
 //    }
     
     private var urlString: String = ""
